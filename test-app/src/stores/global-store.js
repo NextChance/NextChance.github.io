c620b5a034ac82ebc20782d8d8e1917b0d8bc447
Edu Aguilar <eduardo.aguilar@nc43tech.com>
2022-08-05 09:13:46 +0200

    update global-store to add firebase methods to login

--- test-app/src/stores/global-store.js
+++ test-app/src/stores/global-store.js
@@ -1,4 +1,13 @@
 import { defineStore } from "pinia";
+import { initializeApp } from "firebase/app";
+import {
+  getAuth,
+  GoogleAuthProvider,
+  signInWithPopup,
+  signInWithRedirect,
+  getRedirectResult,
+} from "firebase/auth";
+import { signOut } from "@firebase/auth";
 
 const API_BASE_URL = "https://dev-api.cazandogangas.com/v1";
 
@@ -6,6 +15,8 @@ export const useGlobalStore = defineStore("global", {
   state: () => ({
     clientAppToken: null,
     reviews: [],
+    firebaseInstance: null,
+    user: null,
   }),
   getters: {
     getReviews: (state) => state.reviews,
@@ -14,8 +25,8 @@ export const useGlobalStore = defineStore("global", {
   actions: {
     async loginApp() {
       var body = {
-        key: "cbfb63b5737a4e2fba3612582ea5b182",
-        secret: "bd2101d1a1744785ae57ca2f09cfecac",
+        key: import.meta.env.VITE_APP_KEY,
+        secret: import.meta.env.VITE_APP_SECRET,
       };
 
       const request = await fetch(`${API_BASE_URL}/client-app-credentials`, {
@@ -57,6 +68,70 @@ export const useGlobalStore = defineStore("global", {
       } else {
         this.reviews.push(...data.externalReviews);
       }
+    },
+
+    createFirebaseInstance() {
+      const firebaseConfig = {
+        apiKey: import.meta.env.VITE_API_KEY,
+        authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+        projectId: import.meta.env.VITE_PROJECT_ID,
+        storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+        messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+        appId: import.meta.env.VITE_APP_ID,
+        measurementId: import.meta.env.VITE_MEASUREMENT_ID,
+      };
+
+      this.firebaseInstance = initializeApp(firebaseConfig);
+    },
+
+    async googleLoginWithModal() {
+      if (!this.firebaseInstance) {
+        this.createFirebaseInstance();
+      }
+
+      const provider = new GoogleAuthProvider();
+      const auth = getAuth();
+      signInWithPopup(auth, provider)
+        .then((result) => {
+          const credential = GoogleAuthProvider.credentialFromResult(result);
+          const token = credential.accessToken;
+          const user = result.user;
+          console.log(token);
+          console.log(user);
+          this.user = user;
+        })
+        .catch((error) => {
+          console.log(error);
+          const credential = GoogleAuthProvider.credentialFromError(error);
+          console.log(credential);
+          alert("error login with google");
+        });
+    },
+
+    async googleLoginWithRedirection() {
+      if (!this.firebaseInstance) {
+        this.createFirebaseInstance();
+      }
+
+      const provider = new GoogleAuthProvider();
+      const auth = getAuth();
+      await signInWithRedirect(auth, provider);
+    },
+
+    async getLoginDataFromRedirection() {
+      if (!this.firebaseInstance) {
+        this.createFirebaseInstance();
+      }
+
+      const auth = getAuth();
+      const result = await getRedirectResult(auth);
+      this.user = result.user;
+    },
+
+    async firebaseLogout() {
+      const auth = getAuth();
+      await signOut(auth);
+      location.reload();
     },
   },
 });
